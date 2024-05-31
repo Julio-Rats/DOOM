60d6259a6510ae2c16c082ea18ffafd0cfe6b195
Julio-Rats <juliocesarifmg@gmail.com>
2024-05-31 20:00:32 -0300

    Massive code review
    Preparing code to compile and run on current Linux, some modifications and engine customizations

---
+++ linuxdoom-1.10/bin/README.txt
@@ -0,0 +1,1 @@
+# Path to Doom executable and shareware Doom 1
--- linuxdoom-1.10/p_pspr.h
+++ linuxdoom-1.10/p_pspr.h
@@ -67,10 +67,10 @@ typedef enum
 
 typedef struct
 {
-    state_t*	state;	// a NULL state means not active
-    int		tics;
-    fixed_t	sx;
-    fixed_t	sy;
+    state_t* state;	// a NULL state means not active
+    int      tics;
+    fixed_t  sx;
+    fixed_t  sy;
 
 } pspdef_t;
 
